483dc8cdf44a97c91776e0e7a374db0aaf16dfe2
RuslanDabysov <ruslandabysov@gmail.com>
2022-11-15 21:51:54 +0300

    add time_interval on random cards

--- src/components/services/MarvelService.js
+++ src/components/services/MarvelService.js
@@ -17,8 +17,8 @@ class MarvelService {
     getAllCharacters = async () => {
         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=670&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
-
     }
+
 
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
@@ -29,7 +29,7 @@ class MarvelService {
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description ? `${char.description(0, 200)}...` : 'There is no description yet',
+            description: char.description ? `${char.description.slice(0, 200)}...` : 'There is no description yet',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
