0c346bb9543c98e15c4687bf8f583239aa01b145
RuslanDabysov <ruslandabysov@gmail.com>
2022-11-19 11:24:07 +0300

    add some features with checking error

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react/cjs/react.development';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
@@ -103,6 +105,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
